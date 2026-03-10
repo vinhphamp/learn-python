a7e20fa8c68f98a761549561ff4148e005de151a
vinhphamp <134673953+vinhphamp@users.noreply.github.com>
2026-03-10 16:24:29 +0000

    add file to create class MonHoc

--- hello.py
+++ hello.py
@@ -1,1 +1,34 @@
-print("Hello Vinh, welcome to Python!")
+class MonHoc:
+    def __init__(self, maMH, tenMH, soTC):
+        self.maMH = maMH
+        self.tenMH = tenMH
+        self.soTC = soTC
+
+    def hienthiMH(self):
+        print("Mã môn học: ", self.maMH)
+        print("Tên môn học: ", self.tenMH)
+        print("Số tín chỉ: ", self.soTC)
+
+dsMH = []
+
+def nhapDSMH():
+    n = int(input("Nhập vào số lượng môn học: "))
+    for i in range(n):
+        maMH = input("Nhập vào mã môn học: ")
+        tenMH = input("Nhập vào tên môn học: ")
+        soTC = input("Số tín chỉ: ")
+        mh = MonHoc(maMH, tenMH, soTC)
+        dsMH.append(mh)
+
+def hienthiDSMH():
+    print("Danh sách môn học là: ")
+    for i in range(len(dsMH)):
+        print("Thông tin môn học: ", i + 1)
+        dsMH[i].hienthiMH()
+
+def main():
+    nhapDSMH()
+    hienthiDSMH()
+
+
+main()    
